a1078cefcca850e2ede201276aec34b3e43d45f3
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-07-31 20:13:50 +0800

    structure

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog System | about</title>
+</head>
+<body>
+    <h1>About</h1>
+    <h2>{{ $name }}</h2>
+    <p>{{ $email }}</p>
+    <img src="{{ $img }}" alt="Profile Image">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "zulhusni",
+        "email" => "aa@gmail.com",
+        "img" => "https://irp.cdn-website.com/93aa737e/dms3rep/multi/hacker-computer-systems.jpg"
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
