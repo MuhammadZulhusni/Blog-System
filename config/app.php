4be7d457eace3320158ecd8642105009f9b3af45
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-08-02 22:30:58 +0800

    Change Faker to Malay Language

--- config/app.php
+++ config/app.php
@@ -82,7 +82,8 @@ return [
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('FAKER_LOCALE', 'en_US'),
+    // Lepas siap setup yg ni^ pergi .env file tmbh ni 'FAKER_LOCALE=ms_MY'
 
     /*
     |--------------------------------------------------------------------------
