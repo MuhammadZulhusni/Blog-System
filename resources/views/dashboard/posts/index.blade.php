d53407caacf23290cd6f395bfff3f798a0cf9827
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-08-08 01:05:23 +0800

    Display data post based on user login

--- resources/views/dashboard/posts/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('dashboard.layouts.main')
-
-@section('container')
-    <div class="py-6">
-        <h1 class="text-2xl font-bold text-gray-900 mb-2">My Posts</h1>
-        <!-- Divider Line -->
-        <hr class="border-gray-300 mb-4">
-    </div>
-@endsection
