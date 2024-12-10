1ee47ff41c49dd6f827bdf4dfe2a555773fea111
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-12-10 18:26:56 +0800

    Validation form create & Display error message

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -32,7 +32,12 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
     }
 
     /**
