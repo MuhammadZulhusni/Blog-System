7182e0d72872d197071b7ddc3ea2cc0ba6e64d6e
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-12-13 14:44:03 +0800

    Step to upload

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -33,6 +33,8 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
+        return $request->file('image')->store('post->images');
+
         $validatedData = $request->validate([
             'title' => 'required|max:255',
             'slug' => 'required|unique:posts',
