cac5385b1b6e287a8ab498a53f38f892f26ceae2
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-12-13 16:26:44 +0800

    Buat field image on table post

--- database/migrations/2024_08_01_034025_create_posts_table.php
+++ database/migrations/2024_08_01_034025_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id');
             $table->string('title');
             $table->string('slug')->unique(); // ->unique() means x boleh ade slug yang sama
+            $table->string('image')->nullable();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamp('published_at')->nullable();
